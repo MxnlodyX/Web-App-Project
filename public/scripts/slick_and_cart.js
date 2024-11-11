10c508a61ed0e943e732784cbca5a97abd9dde9c
Wattanachai Boonchai <wattanachai.bon@student.mahidol.ac.th>
2024-11-12 05:39:39 +0700

    Front D1 Commit

---
+++ public/scripts/slick_and_cart.js
@@ -0,0 +1,35 @@
+function Slider_Carousel (){
+  $(document).ready(function(){
+    $('.Carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+      });
+    });
+}
+function Cart_detail(){
+  document.addEventListener("DOMContentLoaded", function () {
+    const cart = document.querySelector(".Shopping-Cart");
+    const orderDetails = document.querySelector(".order-details");
+    cart.addEventListener("mouseenter", () => {
+        orderDetails.classList.add("show");
+    });
+    cart.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            orderDetails.classList.remove("show");
+        }, 300);
+    });
+  });
+}
+function use_function(){
+  Slider_Carousel();
+  Cart_detail();
+}
+use_function();
